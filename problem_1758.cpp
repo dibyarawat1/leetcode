b87fd69af0326a2a8f63a31d372287bae63e894d
dibyarawat1 <dibyarawat1@gmail.com>
2026-03-05 17:11:50 +0530

    commit

--- problem_1758.cpp
+++ problem_1758.cpp
@@ -1,33 +1,38 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class solution{
-    public:
-    int minOperations(string s){
-         if(s.size()==1){
+
+class solution {
+public:
+    int minOperations(string s) {
+        if (s.size() == 1) {
             return 0;
-         }
-          int count = 0 ;
-    for(int  i = 1 ; i<s.size() ; i++){
-        if(s[i]==s[i-1]){
-            if(s[i]=='0'){
-                s[i]= '1';
-                count++;
-            }else{
-                s[i]= '0';
-                count++;
+        }
+        
+        int count = 0;
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == s[i-1]) {
+                if (s[i] == '0') {
+                    s[i] = '1';
+                    count++;
+                } else {
+                    s[i] = '0';
+                    count++;
+                }
             }
         }
-}
-if(count < ((s.size())-count)){
-    return count;
+        
+        if (count < (s.size() - count)) {
+            return count;
+        }
+        return s.size() - count;
     }
-    return s.size()-count;
-}
 };
-int main(){
+
+int main() {
     string s;
-    cin>>s;
+    cin >> s;
     solution myobj;
-  cout<<myobj.minOperations(s)<<endl;
-}
+    cout << myobj.minOperations(s) << endl;
+    return 0;
+  }
