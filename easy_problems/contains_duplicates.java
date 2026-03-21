c4bf35c13302fa6171c020138d138e3aa75ee219
dibyarawat1 <dibyarawat1@gmail.com>
2026-03-21 06:59:57 +0530

    commit

---
+++ easy_problems/contains_duplicates.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.*;
+public class contains_duplicates {
+   static class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int count = 0;
+        boolean y = false;
+        Arrays.sort(nums);
+        for(int i = 1 ; i < nums.length ; i++){
+            if(nums[i-1] == nums[i]){
+                count++;
+            }
+            if(count == 1){
+                y = true;
+                
+            }
+        }
+        return  y;
+    }
+}
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] nums = new  int[n];
+    for(int i = 0 ; i< n ; i++){
+        nums[i] = sc.nextInt();
+    }
+    Solution myobj = new Solution();
+     System.out.println( myobj.containsDuplicate(nums));
+}
+}
