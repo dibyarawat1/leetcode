5d95a496c1e0d5f7ccf5099ab2b851fbbd938a68
dibyarawat1 <dibyarawat1@gmail.com>
2026-04-01 13:42:20 +0530

    commit

--- medium_problems/maximum_product_subarray.java
+++ medium_problems/maximum_product_subarray.java
@@ -1,28 +1,26 @@
-import java .util.*;
-public class subarray_product_less_than_k {
+import java.util.Scanner;
+public class maximum_product_subarray {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int count =0;
         int[] nums = new int[n];
         for(int i = 0 ; i < n ; i++){
             nums[i] = sc.nextInt();
         }
-        int k = sc.nextInt();
+        int maxProduct = Integer.MIN_VALUE;
         for(int i = 0 ; i < n ; i++){
             int right = n;
             int left = i;
             int product = 1;
             while(left < right){
                 product = product * nums[left];
-                if(product < k){
-                    count++;
+                if(product > maxProduct){
+                   maxProduct = product;
                 }
                 left++;
             }
         }
-        System.out.println(count);
+        System.out.println(maxProduct);
         sc.close();
-    }
 }
-
+}
