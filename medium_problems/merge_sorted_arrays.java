59f6315e4611e304f63714fc72488ec0e43f1289
dibyarawat1 <dibyarawat1@gmail.com>
2026-04-15 06:36:32 +0530

    commit

---
+++ medium_problems/merge_sorted_arrays.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] n1, int m, int[] n2, int n) {
+       int left = m+n -1 ; 
+       int right = n-1;
+       int i= m-1;
+       while(i >= 0 && right >=0){
+        if(n1[i] < n2[right] ){
+            n1[left] = n2[right];
+            left--;
+            right--;
+        }else {
+            n1[left] = n1[i];
+            left--;
+            i--;
+        }
+       }
+       while(right >= 0){
+        n1[left] = n2[right];
+        right--;
+        left--;
+       }
+        }
+    }
