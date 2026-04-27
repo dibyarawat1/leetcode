f2ed62eb38892e051ef198ccbdca8f66e2c2bea3
dibyarawat1 <dibyarawat1@gmail.com>
2026-04-27 07:31:53 +0530

    commit

---
+++ easy_problems/mostCommonWords.java
@@ -0,0 +1,34 @@
+public class mostCommonWord{
+      public String mostCommonWord(String paragraph, String[] banned){
+        String s ="";
+        int max = Integer.MIN_VALUE;
+    int count = 0 ; 
+    paragraph = paragraph.toLowerCase();
+    String[] arr1 = paragraph.split("[\\s\\p{Punct}]+");
+    for(int i = 0 ; i< arr1.length ; i++){
+        if(arr1[i].isEmpty()){
+            continue;
+        }
+        boolean isBanned = true;
+        for(int k = 0 ; k < banned.length ; k++){
+         if(arr1[i].equals(banned[k])){
+            isBanned = false;
+            break;
+         }
+        }
+        if(isBanned){
+            count = 0 ; 
+            for(int j = 0 ; j< arr1.length ; j++){
+                if(arr1[i].equals(arr1[j])){
+                    count++;
+                }
+            }
+            if(count > max){
+                max = count;
+                s = arr1[i];
+            }
+        }
+    }
+    return s;
+    }
+}
