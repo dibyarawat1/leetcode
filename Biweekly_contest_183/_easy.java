60af9bbc97024b0bf42a8b71b5c4d6e80450471c
dibyarawat1 <dibyarawat1@gmail.com>
2026-05-27 07:23:43 +0530

    contest 1

---
+++ Biweekly_contest_183/_easy.java
@@ -0,0 +1,21 @@
+public class _easy {
+//   3936. Minimum Swaps to Move Zeros to End
+    static int minimumSwaps(int[] arr){
+        int  i = 0;
+        int  j = arr.length -1;
+        int count = 0;
+        while(i < j){
+            if(arr[i] == 0 && arr[j] !=0){
+                count++;
+                i++;
+                j--;
+            }else if(arr[i]== 0 && arr[j] == 0){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return count;
+    }
+    }
+
